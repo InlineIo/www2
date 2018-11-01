7fa10049a1e9453b631748fdc4906f1fd5aef0ff
hgarcia <hernan@dynamicprogrammer.com>
2018-11-01 15:44:04 -0400

    [HG] adding eslint

--- models/users.js
+++ models/users.js
@@ -1,20 +1,20 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const uuid = require("uuid");
-  const users = sequelize.define('users', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    pwd: DataTypes.STRING,
-    salt: DataTypes.STRING,
-    organizationsId: DataTypes.UUID,
-    role: DataTypes.STRING
-  }, {});
-  users.associate = function (models) {
+  const uuid = require("uuid"),
+    users = sequelize.define("users", {
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      pwd: DataTypes.STRING,
+      salt: DataTypes.STRING,
+      organizationsId: DataTypes.UUID,
+      role: DataTypes.STRING
+    }, {});
+  users.associate = (models) => {
     users.belongsTo(models.organizations);
     // associations can be defined here
   };
-  users.beforeCreate((user, _) => {
+  users.beforeCreate((user) => {
     user.id = uuid();
     return user;
   });
--- modules/session/server/routes/api.js
+++ modules/session/server/routes/api.js
@@ -5,14 +5,16 @@ module.exports = (api, db) => {
     if (req.body.password !== req.body.confirmPassword) {
       res.status(409)
         .send({
-        errorCode: "PWD_NO_MATCH"
-      });
+          errorCode: "PWD_NO_MATCH"
+        });
       return;
     }
 
     db.users.findOne({
-      where: { email: req.body.email },
-      attributes: ['id']
+      where: {
+        email: req.body.email
+      },
+      attributes: ["id"]
     }).then((usr) => {
       if (usr) {
         res.status(409)
@@ -21,16 +23,16 @@ module.exports = (api, db) => {
             errors: [usr]
           });
       } else {
-        db.organizations.create({ name: req.body.organization })
+        db.organizations.create({name: req.body.organization})
           .then((org) => {
             const newUser = req.body;
             newUser.salt = bcrypt.genSaltSync();
-            newUser.organizationsId = org.id;
+            newUser.orgId = org.id;
             newUser.pwd = bcrypt.hashSync(newUser.password, newUser.salt);
-            return db.users.create(newUser)
+            return db.users.create(newUser);
           })
           .then(() => {
-            res.send({ status: "OK" });
+            res.send({status: "OK"});
           })
           .catch((error) => {
             if (error.name === "SequelizeUniqueConstraintError") {
@@ -39,7 +41,7 @@ module.exports = (api, db) => {
                   errorCode: "USR_EXISTS",
                   errors: error.errors
                 });
-                return
+                return;
               }
               if (error.parent && error.parent.constraint === "organizations_name_key") {
                 res.status(409).send({

2b321b2a12a0ea18f404538c1723b488b04d70ae
hgarcia <hernan@dynamicprogrammer.com>
2018-11-01 14:44:09 -0400

    [HG] do not create an org if usr already exists.

--- modules/session/server/routes/api.js
+++ modules/session/server/routes/api.js
@@ -1,6 +1,7 @@
 module.exports = (api, db) => {
   api.post("/signup", (req, res) => {
     const bcrypt = require("bcrypt");
+
     if (req.body.password !== req.body.confirmPassword) {
       res.status(409)
         .send({
@@ -8,35 +9,49 @@ module.exports = (api, db) => {
       });
       return;
     }
-    db.organizations.create({name: req.body.organization})
-      .then((org) => {
-        const newUser = req.body;
-        newUser.salt = bcrypt.genSaltSync();
-        newUser.orgId = org.id;
-        newUser.pwd = bcrypt.hashSync(newUser.password, newUser.salt);
-        return db.users.create(newUser)
-      })
-      .then(() => {
-        res.send({ status: "OK" });
-      })
-      .catch((error) => {
-        if (error.name === "SequelizeUniqueConstraintError") {
-          if (error.parent && error.parent.constraint === "users_email_key") {
-            res.status(409).send({
-              errorCode: "USR_EXISTS",
-              errors: error.errors
-            });
-            return
-          }
-          if (error.parent && error.parent.constraint === "organizations_name_key") {
-            res.status(409).send({
-              errorCode: "ORG_EXISTS",
-              errors: error.errors
-            });
-            return;
-          }
-        }
-        res.status(500).send(error);
-      });
+
+    db.users.findOne({
+      where: { email: req.body.email },
+      attributes: ['id']
+    }).then((usr) => {
+      if (usr) {
+        res.status(409)
+          .send({
+            errorCode: "USR_EXISTS",
+            errors: [usr]
+          });
+      } else {
+        db.organizations.create({ name: req.body.organization })
+          .then((org) => {
+            const newUser = req.body;
+            newUser.salt = bcrypt.genSaltSync();
+            newUser.orgId = org.id;
+            newUser.pwd = bcrypt.hashSync(newUser.password, newUser.salt);
+            return db.users.create(newUser)
+          })
+          .then(() => {
+            res.send({ status: "OK" });
+          })
+          .catch((error) => {
+            if (error.name === "SequelizeUniqueConstraintError") {
+              if (error.parent && error.parent.constraint === "users_email_key") {
+                res.status(409).send({
+                  errorCode: "USR_EXISTS",
+                  errors: error.errors
+                });
+                return
+              }
+              if (error.parent && error.parent.constraint === "organizations_name_key") {
+                res.status(409).send({
+                  errorCode: "ORG_EXISTS",
+                  errors: error.errors
+                });
+                return;
+              }
+            }
+            res.status(500).send(error);
+          });
+      }
+    });
   });
 };

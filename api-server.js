dd716946d07c53f99cdda0df954ca67d34614080
hgarcia <hernan@dynamicprogrammer.com>
2018-11-02 16:51:23 -0400

    [hg] adding swagger

--- api-server.js
+++ api-server.js
@@ -1,7 +1,19 @@
 const express = require("express"),
   session = require("./session-store"),
-  api = express();
+  createMiddleware = require("swagger-express-middleware"),
+  api = express(),
+  path = require("path"),
+  swaggerFile = path.join(__dirname, "InlineApi.yaml");
 
+createMiddleware(swaggerFile, api, (_, middleware) => {
+  api.use(
+    middleware.metadata(),
+    middleware.files(),
+    middleware.parseRequest(),
+    middleware.validateRequest(),
+    middleware.mock()
+  );
+});
 api.use(express.json());
 session.addSession(api);
 
